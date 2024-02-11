3a546f399e5f73093cad412ff9a3e4db741525b7
Марія Трофімова (ЗІПЗ-22-1) <153320412+MariyaTro@users.noreply.github.com>
2024-02-11 23:07:15 +0200

    23:07
    11/02/2024

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть перше число:");
+        string input1 = Console.ReadLine();
+
+        Console.WriteLine("Введіть друге число:");
+        string input2 = Console.ReadLine();
+
+        if (double.TryParse(input1, out double number1) && double.TryParse(input2, out double number2))
+        {
+            double sum = number1 + number2;
+            Console.WriteLine($"Сума чисел {number1} та {number2} дорівнює: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Некоректне введення. Будь ласка, введіть числа.");
+        }
+
+        Console.WriteLine("Натисніть будь-яку клавішу для виходу...");
+        Console.ReadKey();
+    }
+}
